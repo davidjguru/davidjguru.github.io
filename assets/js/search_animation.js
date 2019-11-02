f6bde06cd5095ded45c8225253c1bd55f3a065a9
David Rodríguez <davidjguru@gmail.com>
2019-11-03 00:23:35 +0100

    (CSS) / (JS) - Add animation to search box.

---
+++ assets/js/search_animation.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("search-box");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+    searchBtn.classList.toggle("close");
+    input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
